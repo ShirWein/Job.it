710d8a8e429d59f22edbd251b5f8f65ac1010a83
Shir W <shirw@Shirs-MacBook-Pro.local>
2023-05-13 15:58:57 +0300

    Charts Component

---
+++ components/Charts.jsx
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'; 
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const Charts = () => {
+    // This is the data for the char !!!
+    const [chartData, setChartData] = useState({
+        datasets: []
+    });
+
+    const [chartOptions, setChartOptions] = useState({});
+
+
+    // Static Data For Example!!
+    useEffect(() => {
+        setChartData({
+            labels: ['Saved', 'Applied', 'Interview', 'Offer'],
+            datasets: [{
+                label: 'Job Stage',
+                data: [10, 50, 0, 0],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgb(53, 162, 235, 0.4)',
+            
+            }
+
+            ]
+        })
+        // Chart design
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'right',
+                },
+                title: {
+                    display: true,
+                    text: 'Job Stage'
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+
+        })
+    }, [])
+
+  return (
+    <>
+        <div className='w-full md:col-span-3 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
+            <Bar data={chartData} options={chartOptions}/> 
+
+        </div>
+    </>
+  )
+}
+
+export default Charts
---
+++ components/Tasks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tasks = () => {
+  return (
+    <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>Tasks</div>
+  )
+}
+
+export default Tasks
